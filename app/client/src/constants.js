4bfbc56e070813cf4b00b9db6e29d380be28a27e
SleepyJen <jennifer.ni0924@gmail.com>
2022-08-14 16:31:56 -0700

    modified dashboard

--- app/client/src/constants.js
+++ app/client/src/constants.js
@@ -5,7 +5,7 @@ angular.module('reg')
         NAME: 'HackMIT 2015',
     })
     .constant('DASHBOARD', {
-        UNVERIFIED: 'You should have received an email asking you verify your email. Click the link in the email and you can start your application!',
+        UNVERIFIED: 'Once you have be approved, please look out for an email for Dinner option menu!',
         INCOMPLETE_TITLE: 'You still need to complete your application!',
         INCOMPLETE: 'If you do not complete your application before the [APP_DEADLINE], you will not be considered for the admissions lottery!',
         SUBMITTED_TITLE: 'Your application has been submitted!',
@@ -18,6 +18,6 @@ angular.module('reg')
         CONFIRMED_NOT_PAST_TITLE: 'You can edit your confirmation information until [CONFIRM_DEADLINE]',
         DECLINED: 'We\'re sorry to hear that you won\'t be able to make it to HackMIT 2015! :(\nMaybe next year! We hope you see you again soon.',
     })
-    .constant('TEAM',{
+    .constant('TEAM', {
         NO_TEAM_REG_CLOSED: 'Unfortunately, it\'s too late to enter the lottery with a team.\nHowever, you can still form teams on your own before or during the event!',
     });
